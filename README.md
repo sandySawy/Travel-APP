c25e8134392ddcd70163da0c32d9c6b77dd91f68
sandySawy <73387790+sandySawy@users.noreply.github.com>
2021-01-28 22:42:24 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-Travel APP
+#Travel APP
+
+##introduction
+This application allows the user to enter the date and location of his flight, and tell him the weather on 
+this date, and the image of the city
+
+## API used 
+1.Pixabay
+2.GeoNames
+3.WeatherBit
+
+## Instructions
+`npm install`  **run this to install all required packages**
+
+`npm run test`  ** for testing**
+
+`npm run dev`  **for creating development build**
+
+`npm run build`  ** for creating production build**
---
+++ webpack.dev.js
@@ -0,0 +1,41 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+    entry: './src/client/index.js',
+    mode: 'development',
+    devtool: 'source-map',
+    output: {
+      libraryTarget: 'var',
+      library: 'Client'
+    },
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader", "sass-loader"],
+              },
+            
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                loader: 'file-loader',
+                options: {
+                  name: '[path][name].[ext]',
+                         },
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+          }),
+
+    ]
+}
